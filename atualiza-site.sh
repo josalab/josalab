1d56ee1ab9cfa436b14fe03afc59714eec1670cc
Jurandy Soares <jurandy.soares@gmail.com>
2021-03-31 21:23:50 -0300

    Melhoria no script de atualização do site

--- atualiza-site.sh
+++ atualiza-site.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
 
 git pull
+
 cd ../josalab.github.io/
+
 git pull
+git merge
+git add .
+git commit -m 'removido'
+
 mkdocs gh-deploy --config-file ../josalab/mkdocs.yml --remote-branch main
+
 read -p "Pressione ENTER para sair."
