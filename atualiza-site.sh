b5fd78b4db6643efd72827a3f99bb35b7676af76
fredericohorie <81012362+fredericohorie@users.noreply.github.com>
2022-02-08 16:42:11 -0300

    Atualização do código fonte em ter,  8 de fev de 2022 16:42:11'

--- atualiza-site.sh
+++ atualiza-site.sh
@@ -4,14 +4,16 @@ git pull
 git add .
 git commit -m "Atualização do código fonte em $(date)'"
 git push
+mkdocs gh-deploy
 
-cd ../josalab.github.io/
 
-git pull
-git merge
-git add .
-git commit -m 'removido'
+# cd ../josalab.github.io/
 
-mkdocs gh-deploy --config-file ../josalab/mkdocs.yml --remote-branch main
+# git pull
+# git merge
+# git add .
+# git commit -m 'removido'
+
+# mkdocs gh-deploy --config-file ../josalab/mkdocs.yml --remote-branch main
 
 #read -p "Pressione ENTER para sair."
